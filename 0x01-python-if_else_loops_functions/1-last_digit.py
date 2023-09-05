2d9ba5d9ac1e2093a03418a3a566b3bf9a8b270f
codinBabe <teboola2@gmail.com>
2023-09-05 12:10:57 +0000

    fix error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,13 @@ number = random.randint(-10000, 10000)
 Last_digit = abs(number) % 10
 
 if number < 0:
-	Last_digit = -Last_digit
+Last_digit = -Last_digit
 
 print("Last digit of", number, "is", Last_digit, end=" ")
 
 if Last_digit < 6 and Last_digit != 0:
-	print("is less than 6 and not 0")
+print("and is less than 6 and not 0")
 elif Last_digit > 5:
-	print("and is greater than 5")
+print("and is greater than 5")
 else:
-	print("and is 0")
+print("and is 0")
