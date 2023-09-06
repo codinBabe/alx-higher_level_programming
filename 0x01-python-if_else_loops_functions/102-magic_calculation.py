c3e5959fc9377dc5575d0b017aa5be0f792fa9da
codinBabe <teboola2@gmail.com>
2023-09-06 06:16:32 -0700

    handle rem cases

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b, c):
-    if a >= b and c <= b:
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
         return a * b - c
