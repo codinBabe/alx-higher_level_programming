caa5fb5e1e17c69ac19004c1350329d6b4b24fec
codinBabe <teboola2@gmail.com>
2023-09-07 10:11:59 -0700

    add files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,10 +4,7 @@ if __name__ == "__main__":
 
     from sys import argv
 
-    if len(argv) == 1:
-        print("0")
-    else:
-        res = 0
-        for i in argv[1:]:
-            res += int(i)
-            print(res)
+    res = 0
+    for i in argv[1:]:
+        res += int(i)
+        print(res)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == '__main__':
     import hidden_4
 
-    a = dir
-    for i in range(0, len(a)):
-        if a[i][0:2] != "__":
-            print("{}".format(a[i]))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
