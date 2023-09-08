eacd779b5267e717ef4a74a35a011ebca678b784
codinBabe <teboola2@gmail.com>
2023-09-07 17:50:18 -0700

    fix erro

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     from sys import argv
 
-    res = 0
-    for i in argv[1:]:
-        res += int(i)
+    if len(argv) == 0:
+        print("0")
+    else:
+        res = 0
+        for i in argv[1:]:
+            res += int(i)
         print(res)
