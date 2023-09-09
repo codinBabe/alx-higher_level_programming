33538225d3e3b2cbb6249799d83bc3c179f6c518
codinBabe <teboola2@gmail.com>
2023-09-09 02:22:39 -0700

    update task 100 and fix pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,30 +2,26 @@
 if __name__ == "__main__":
     from sys import argv, exit
 
-    if len(argv) - 1 != 3:
+    res = len(argv) - 1
+    if res != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-        operator = argv[2]
-        if operator != '+' and operator != '-' and operator != '*' and operator != '/':
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    opt = argv[2]
+    if opt != '+' and opt != '-' and opt != '*' and opt != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-        from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
-        a = int(argv[1])
-        b = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-        if operator == '+':
-            print("{} + {} = {}".format(a, b, add(a,b)))
-        elif operator == '-':
-            print("{} - {} = {}".format(a, b, sub(a,b)))
-        elif operator == '*':
-            print("{} * {} = {}".format(a, b, mul(a,b)))
-        elif operator == '/':
-            if b == 0:
-                exit(1)
-            print("{} / {} = {}".format(a, b, div(a,b)))
-
-
-
+    if opt == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif opt == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif opt == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
