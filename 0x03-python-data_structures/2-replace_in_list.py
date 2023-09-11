992266b34ecf7a8ddc2b85a4572674de4098b56f
codinBabe <teboola2@gmail.com>
2023-09-11 06:33:55 -0700

    fix error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,11 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 and idx > len(my_list) - 1:
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = element
+        my_list.pop(idx)
+        my_list.insert(idx, element)
         return my_list
