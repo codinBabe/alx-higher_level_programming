a278b48baa789affe682f11962b0ba4f865545af
codinBabe <teboola2@gmail.com>
2023-09-11 10:28:57 -0700

    add task 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
         for n in r:
-            print("{:d}".format(n), end=" ")
+            print("{:d}".format(n), end=" " if n != r[-1] else "")
         print()
