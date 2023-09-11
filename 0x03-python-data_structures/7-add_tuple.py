7c808eb9f9c48933b1658891b15cfe0639d91ac2
codinBabe <teboola2@gmail.com>
2023-09-11 10:45:00 -0700

    added task 8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def add_tuple(tuple_a=(), tuple_b=()):
     a1 = tuple_a + (0, 0)
     b2 = tuple_b + (0, 0)
@@ -11,4 +10,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     res1 += a1[0] + b2[0]
     res2 += a1[1] + b2[1]
 
-    return(res1, res2)
+    return (res1, res2)
