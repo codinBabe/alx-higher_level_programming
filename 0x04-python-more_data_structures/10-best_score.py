1e6c43b8bc1682cb06a540df1694ee101772763c
codinBabe <teboola2@gmail.com>
2023-09-13 13:12:11 -0700

    added task 11

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary == None or a_dictionary == {}:
+        return None
+
+    v = max(a_dictionary, key=lambda a: a_dictionary[a])
+    return v
