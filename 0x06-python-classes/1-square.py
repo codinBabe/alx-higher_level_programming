39fa828e9be9d873142f65d9b152087809945d62
codinBabe <teboola2@gmail.com>
2023-09-26 12:29:56 -0700

    added sol to task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """square class with a private attribute"""
+    def __init__(self, size):
+        self.__size = size
