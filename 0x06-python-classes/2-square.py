f5302a72907a52260e040dc6dc551db810405e57
codinBabe <teboola2@gmail.com>
2023-09-26 12:46:14 -0700

    added task 2 sol

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """square class with private attribute and exception"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
