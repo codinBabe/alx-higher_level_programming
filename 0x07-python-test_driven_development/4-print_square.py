bb3b16ed87a19a7701ee568a2c37b0b4128cf5ac
codinBabe <teboola2@gmail.com>
2023-10-03 10:05:13 -0700

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""a function that prints a square with the character #"""
+
+
+def print_square(size):
+    """This function print '#' in square
+    :param: size of type int
+    :return: '#' of size
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    size_str = ""
+    for i in range(size):
+        size_str += "#" * size + "\n"
+    print(size_str)
