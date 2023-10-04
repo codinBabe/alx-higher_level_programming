12618b8c80db5d584b7c6c7ece075e1a48fa1276
codinBabe <teboola2@gmail.com>
2023-10-04 02:23:12 -0700

    maxinteger

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -21,5 +21,25 @@ class TestMaxInteger(unittest.TestCase):
         maxbegin = [4, 1, 3, 2]
         self.assertEqual(max_integer(maxbegin), 4)
 
+    def test_middle(self):
+        """check a list with max value in the middle"""
+        middle = [1, 2, 8, 3, 4]
+        self.assertEqual(max_integer(middle), 8)
+
+    def test_neg_num(self):
+        """Test negative number"""
+        neg_num = [-5, -6, -111]
+        self.assertEqual(max_integer(neg_num), -5)
+
+    def test_one_neg_num(self):
+        """Test one negative number"""
+        one_neg = [-5, 6, 2]
+        self.assertEqual(max_integer(one_neg), 6)
+
+    def one_num(self):
+        """check for one number"""
+        num = [1]
+        self.assertEqual(max_integer(one), 1)
+
 if __name__ == '__main__':
     unittest.main()
