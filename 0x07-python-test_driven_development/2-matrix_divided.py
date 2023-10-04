f6e7da0607668d8168543c6f271db5743bbcea2c
codinBabe <teboola2@gmail.com>
2023-10-04 01:10:08 -0700

    fixing error

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,10 +10,12 @@ def matrix_divided(matrix, div):
     :param div: the integer to div the matrix with of type(int or float)
     :return: the divided matrix of type(int or float)
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list)\
-            for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of \
-                integers/floats")
+    err = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(matrix, list) or not \
+            all(isinstance(row, list) for row in matrix):
+        raise TypeError(err)
+    if not all(isinstance(ele, (int, float)) for row in matrix for ele in row):
+        raise TypeError(err)
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
@@ -23,4 +25,3 @@ def matrix_divided(matrix, div):
 
     res = [[round(ele / div, 2) for ele in row] for row in matrix]
     return res
-
