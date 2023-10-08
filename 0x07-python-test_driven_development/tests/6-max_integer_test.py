91725f9d6cb900afabe018df4b03fb31ae72b523
codinBabe <teboola2@gmail.com>
2023-10-07 22:05:18 -0700

    checking testfiles

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -36,10 +36,35 @@ class TestMaxInteger(unittest.TestCase):
         one_neg = [-5, 6, 2]
         self.assertEqual(max_integer(one_neg), 6)
 
+    def test_ints_and_floats(self):
+        """check a list of ints and floats."""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_float(self):
+        """check a list of floats."""
+        float_lst = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(float_lst), 15.2)
+
+    def test_neg_float(self):
+        """check negative floats"""
+        neg_float = [-5.55, -66.66, -111.1]
+        self.assertEqual(max_integer(neg_float), -5.55)
+
+    def test_string(self):
+        """check for string"""
+        name = "Toyin"
+        self.assertEqual(max_integer(name), 'y')
+
     def one_num(self):
         """check for one number"""
         num = [1]
         self.assertEqual(max_integer(num), 1)
+
+    def test_datatypes(self):
+        """ check for different data types"""
+        with self.assertRaises(TypeError):
+            max_integer([1, "1"])
 
 if __name__ == '__main__':
     unittest.main()
