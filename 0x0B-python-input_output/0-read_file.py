8a44214df59f1d6d4431294b63cc8c30a21e1ae3
codinBabe <teboola2@gmail.com>
2023-10-10 10:58:27 -0700

    write a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""a function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """This function reads a file"""
+    with open(filename, 'r') as file:
+        data = file.read()
+    print(data)
