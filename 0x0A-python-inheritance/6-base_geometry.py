a886b41222d1fecdcd3fb5a6dc1497654cf6376c
codinBabe <teboola2@gmail.com>
2023-10-11 13:36:41 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""A class"""
+
+
+class BaseGeometry:
+    """an empty class"""
+
+def area(self):
+    """a public instance"""
+    raise Exception("area() is not implemented")
+    
