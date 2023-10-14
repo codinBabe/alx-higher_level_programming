6376f8805aff5e77f954f9ed53a52d27c543b97d
codinBabe <teboola2@gmail.com>
2023-10-14 09:56:39 -0700

    dispaly #0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""A class Base"""
+
+
+class Base:
+    """a class with private attribute"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
