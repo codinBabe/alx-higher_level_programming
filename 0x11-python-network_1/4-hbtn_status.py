c43ccfd3b8ee07ea379a6725d2d5dcb8d51b9e8d
codinBabe <teboola2@gmail.com>
2024-03-28 21:48:13 -0700

    task 3-4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    data = req.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(data), data))
